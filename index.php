1dadb7c21da26d04069031d9b15b71421b9b668c
Elodie <simonnetelodie88@gmail.com>
2021-04-08 17:36:18 +0200

    footer integration 2
    adjust social network sizes and add general conditions

--- index.php
+++ index.php
@@ -19,5 +19,6 @@
 				<img src="img/insta.png">
 				<img src="img/mail.png">
 		</div>
+		<p> Ce site est réalisé dans le cadre d'un exercice pédagogique pour la formation d'intégratrice web du CNAM d'Epinal. </p>
 	</footer>
 </html>
