bbed8406ca7f3db652c310c77882bd9496ae9608
Julien <Julien.goisset@gmail.com>
2021-04-09 11:12:03 +0200

    PHP header implementation
    Using PHP include to prompt header on project

--- index.php
+++ index.php
@@ -8,11 +8,7 @@
 		<link rel="stylesheet" type="text/css" href="css/fonts.css">
 	</head>
 	<body>
-		<header>
-			<h1>Voyageurs du monde</h1>
-		    <p>Le voyage, c'est la seule chose qui me coûte de l'argent tout en me rendant plus riche.</p>
-		</header>
-
+		<?php include ('header.php'); ?>
 	</body>
 	<footer>
 
--- page-type.php
+++ page-type.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="css/mise-en-page.css">
 		<link rel="stylesheet" type="text/css" href="css/fonts.css">
 	</head>
-	
+    <?php include ('header.php'); ?>
 	<body>
 
 	</body>
