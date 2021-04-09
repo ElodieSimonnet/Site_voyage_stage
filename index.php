4cbb6ddb633589f766b8f3c1ae1ec771fe6fde80
Julien <Julien.goisset@gmail.com>
2021-04-09 10:38:46 +0200

    Refactor Post Review
    Minor html & css changes, ready to be implemented

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
 	</body>
 	<footer>
 		<div id="reseau-container">
-				<img src="img/facebook.png">
-				<img src="img/insta.png">
-				<img src="img/mail.png">
+            <img src="img/facebook.png">
+            <img src="img/insta.png">
+            <img src="img/mail.png">
 		</div>
 		<p> Ce site est réalisé dans le cadre d'un exercice pédagogique pour la formation d'intégratrice web du CNAM d'Epinal. </p>
 	</footer>
