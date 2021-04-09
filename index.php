ce961d651741992174432db264a4f69a6d2b1b61
Julien <Julien.goisset@gmail.com>
2021-04-09 11:06:29 +0200

    Quick exemple
    Need to be cleaned

--- index.php
+++ index.php
@@ -46,7 +46,25 @@
 			</div>
 		</div>
 
-	
+        <div class="country-container" style="width: 95%; margin:30px auto 30px auto; display: flex; justify-content: space-between">
+            <div class="rectangle_blanc" style="width:20%; background-color: white; border: solid 2px black; z-index: 0; display: flex; position: relative">
+                <img src="img/jap.png" style="width: 96%; margin: 8px auto 38px auto; z-index: 1; position: relative; justify-content: center">
+                <p style="position: absolute; z-index: 2; width: 100%; text-align: center; align-self: flex-end; bottom: 20px; color: white; font-size: 2em">Japon</p>
+            </div>
+            <div class="rectangle_blanc" style="width:20%; background-color: white; border: solid 2px black; z-index: 0; display: flex; position: relative">
+                <img src="img/jap.png" style="width: 96%; margin: 8px auto 38px auto; z-index: 1; position: relative; justify-content: center">
+                <p style="position: absolute; z-index: 2; width: 100%; text-align: center; align-self: flex-end; bottom: 20px; color: white; font-size: 2em">Japon</p>
+            </div>
+            <div class="rectangle_blanc" style="width:20%; background-color: white; border: solid 2px black; z-index: 0; display: flex; position: relative">
+                <img src="img/jap.png" style="width: 96%; margin: 8px auto 38px auto; z-index: 1; position: relative; justify-content: center">
+                <p style="position: absolute; z-index: 2; width: 100%; text-align: center; align-self: flex-end; bottom: 20px; color: white; font-size: 2em">Japon</p>
+            </div>
+            <div class="rectangle_blanc" style="width:20%; background-color: white; border: solid 2px black; z-index: 0; display: flex; position: relative">
+                <img src="img/jap.png" style="width: 96%; margin: 8px auto 38px auto; z-index: 1; position: relative; justify-content: center">
+                <p style="position: absolute; z-index: 2; width: 100%; text-align: center; align-self: flex-end; bottom: 20px; color: white; font-size: 2em">Japon</p>
+            </div>
+        </div>
+
 
 	</body>
 	<footer>
