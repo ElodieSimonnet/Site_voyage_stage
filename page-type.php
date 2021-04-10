77a22393f512962fbd8f8b3073d3d438a4728338
Elodie <simonnetelodie88@gmail.com>
2021-04-10 23:17:40 +0200

    country page japan
    content's structure added

--- page-type.php
+++ page-type.php
@@ -17,6 +17,44 @@
 			<h2> Le pays du soleil levant </h2>
 			<p> Le Japon (en japonais : 日本) est un pays asiatique au large de la Russie et de la Corée dont la capitale est Tokyo. L'archipel japonais couvre 370 000 km². Le Japon est aussi appelé « pays du soleil levant », parce qu'il se trouve sur un archipel situé juste à l'est du centre géographique de l'Asie (le Xinjiang, en Chine). Pays montagneux, le Japon renferme de très nombreux volcans et est régulièrement secoué par des séismes. Ses habitants sont les Japonais, qui sont environ 127 millions. La natalité étant très faible et l'immigration quasi inexistante, la population japonaise commence à diminuer. Très grand pays industriel, le Japon est l'un des États les plus riches et les plus développés du monde, il appartient d'ailleurs à l'organisation G7. Il possède la plus grande dette du monde ; mais elle est détenue en très grande partie par les Japonais.
 			</p>
+			<h3> Gastronomie japonaise </h3>
+				<div id="conteneur-plats">
+						
+						<div id="img-gauche">
+
+
+						</div>
+
+						<div id="img-droite">
+
+
+						</div>
+
+				</div>  
+			<h3> Villes à visiter </h3>
+				<div id="conteneur-villes">
+						
+						<div id="ville">
+
+
+						</div>
+
+						<div id="ville">
+
+
+						</div>
+
+						<div id="ville">
+
+
+						</div>
+
+				</div>
+
+	
+
+
+
 		</div>
 
 
