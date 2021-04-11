837eadce5ac960b394e616049860595dc318d9eb
Elodie <simonnetelodie88@gmail.com>
2021-04-11 04:49:49 +0200

    content food and cities
    html and css added for food and cities with flexbox

--- page-type.php
+++ page-type.php
@@ -14,20 +14,24 @@
 		</div>
 
 		<div id="content">
-			<h2> Le pays du soleil levant </h2>
-			<p> Le Japon (en japonais : 日本) est un pays asiatique au large de la Russie et de la Corée dont la capitale est Tokyo. L'archipel japonais couvre 370 000 km². Le Japon est aussi appelé « pays du soleil levant », parce qu'il se trouve sur un archipel situé juste à l'est du centre géographique de l'Asie (le Xinjiang, en Chine). Pays montagneux, le Japon renferme de très nombreux volcans et est régulièrement secoué par des séismes. Ses habitants sont les Japonais, qui sont environ 127 millions. La natalité étant très faible et l'immigration quasi inexistante, la population japonaise commence à diminuer. Très grand pays industriel, le Japon est l'un des États les plus riches et les plus développés du monde, il appartient d'ailleurs à l'organisation G7. Il possède la plus grande dette du monde ; mais elle est détenue en très grande partie par les Japonais.
-			</p>
+			<div id="presentation">
+				<h2> Le pays du soleil levant </h2>
+				<p> Le Japon (en japonais : 日本) est un pays asiatique au large de la Russie et de la Corée dont la capitale est Tokyo. L'archipel japonais couvre 370 000 km². Le Japon est aussi appelé « pays du soleil levant », parce qu'il se trouve sur un archipel situé juste à l'est du centre géographique de l'Asie (le Xinjiang, en Chine). Pays montagneux, le Japon renferme de très nombreux volcans et est régulièrement secoué par des séismes. Ses habitants sont les Japonais, qui sont environ 127 millions. La natalité étant très faible et l'immigration quasi inexistante, la population japonaise commence à diminuer. Très grand pays industriel, le Japon est l'un des États les plus riches et les plus développés du monde, il appartient d'ailleurs à l'organisation G7.
+				</p>
+			</div>
 			<h3> Gastronomie japonaise </h3>
 				<div id="conteneur-plats">
 						
 						<div id="img-gauche">
-
-
+							<img src="img/takoyaki.png">
+							<h4> Le takoyaki </h4>
+							<p>Mets de la cuisine japonaise classé dans les konamon et une spécialité d'Osaka, se présentant sous forme de boulettes de pâte, semblable à la pâte à crêpe, contenant des morceaux de poulpe, cuites en moule, comme les gaufres. </p>
 						</div>
 
 						<div id="img-droite">
-
-
+							<img src="img/okonomiyaki1.png">
+							<h4> L'okonomiyaki </h4>
+							<p>Ce plat japonais est composé d'une pâte qui enrobe un nombre d'ingrédients très variables découpés en petits morceaux, le tout étant cuit sur une plaque chauffante. Il est souvent servi dans des restaurants spécialisés, où il est parfois possible de le réaliser soi-même.</p>
 						</div>
 
 				</div>  
@@ -35,22 +39,50 @@
 				<div id="conteneur-villes">
 						
 						<div id="ville">
-
-
+							<img src="img/nara.png">
+							<p>Au centre-sud de l'île d'Honshū, Nara est la capitale de la préfecture japonaise du même nom. La ville abrite des œuvres d'art et des temples importants qui remontent au VIIIe siècle, lorsqu'elle était la capitale du Japon.</p> 
 						</div>
 
 						<div id="ville">
-
-
-						</div>
+							<img src="img/okkaido.png">
+							<p>Autrefois appelée Yeso ou Ezo, Hokkaido est la plus septentrionale des quatre îles principales de l'archipel du Japon. Parmi les japonais qui y viventse trouvent une minorité d'Aïnous, qui furent les premiers habitants de l'archipel.</p> 
+ 						</div>
 
 						<div id="ville">
-
-
+							<img src="img/okinawa.png">
+							<p>Ile de l'archipel Ryukyu, Okinawa est une ville dont les habitants soulignent avec force leur passé de royaume indépendant et leur différence culturelle avec le Japon. La région est connue pour ses plages et la beauté de ses eaux.</p> 
 						</div>
 
 				</div>
 
+				<div id="galerie_container">
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+					<div class="rectangle_blanc">
+						<img src="img/jap.png">	
+					</div>
+				</div>
+					
+		</div>
 	
 
 
