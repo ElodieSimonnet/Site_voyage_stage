e821415228a43f2ca54055e72841c79b80cf2972
Elodie <simonnetelodie88@gmail.com>
2021-04-12 12:58:54 +0200

    form first commit

--- form.php
+++ form.php
@@ -9,6 +9,36 @@
 	</head>
     <?php include ('header.php'); ?>
 	<body>
+		<p> Si vous avez une question, vous pouvez me contacter en remplissant ce formulaire, par mail ou sur les réseaux sociaux. </p>
+		<div id="carte_blanche">
+			<div id="photo">
+				<img src="img/carte.png">
+			</div>
+
+			<div id="formulaire">
+			<form> 
+				<legend>Formulaire de contact</legend>  
+  				<article id="case"> 
+    				<label for="name">Votre nom:</label>
+    				<input type="text" id="name" name="user_name" >
+
+    				<label for="mail">Votre e-mail:</label>
+    				<input type="text" required id="mail" name="user_mail" >
+
+    				<label for="objet">Objet:</label>
+    				<input type="text" name="objet" id="objet">
+
+    				<label for="msg">Votre message:</label>
+        			<textarea id="msg" name="user_message"></textarea>
+        		</article>
+
+        		<article>
+    				<button type="submit">ENVOYER</button>
+            	</article>
+    		</form>
+    		</div>
+
+		</div>
 
 	</body>
     <?php include ('footer.php'); ?>
