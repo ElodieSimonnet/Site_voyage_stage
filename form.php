e22bafb3ad6b1335d5a9ac23ce9cee746012468d
Elodie <simonnetelodie88@gmail.com>
2021-04-15 11:51:28 +0200

    relecture et optimisation formulaire
    correction indentation, ajout alt, suppression attributs inutiles, réorganisation

--- form.php
+++ form.php
@@ -11,43 +11,33 @@
 	<body>
 		<p> Si vous avez une question, vous pouvez me contacter en remplissant ce formulaire, par mail ou sur les réseaux sociaux. </p>
 		<div id="carte_blanche">
-
 			<div id="photo">
-				<img src="img/carte.png">
+				<img src="img/carte.png" alt="photo paysage">
 			</div>
-			<div class="trait"></div>
-
-			
+			<div class="trait">
+			</div>
 			<div id="formulaire">
-			<form> 
-
-				<legend>Formulaire de contact</legend> 
-
-  				<article id="case"> 
-    				<label for="name">Votre nom:</label>
-    				<input type="text" id="name" name="user_name" >
-
-    				<label for="mail">Votre e-mail:</label>
-    				<input type="text" required id="mail" name="user_mail" >
-
-    				<label for="objet">Objet:</label>
-    				<input type="text" name="objet" id="objet">
-
-    				<label for="msg">Votre message:</label>
-        			<textarea id="msg" name="user_message"></textarea>
-        		</article>
-
-        		<article>
-    				<button type="submit">ENVOYER</button>
-            	</article>
-    		</form>
-    		<div id="timbre_blanc">
-    		<img src="img/timbre.png">	
+				<form> 
+					<legend>Formulaire de contact</legend> 
+					<article id="case"> 
+    					<label for="name">Votre nom:</label>
+    					<input type="text" required id="name" name="user_name">
+						<label for="mail">Votre e-mail:</label>
+						<input type="text" required id="mail" name="user_mail">
+      					<label for="objet">Objet:</label>
+    					<input type="text" id="objet" name="objet">
+						<label for="msg">Votre message:</label>
+        				<textarea id="msg" name="user_message"></textarea>
+        			</article>
+					<article>
+    					<button type="submit">ENVOYER</button>
+            		</article>
+    			</form>
+    			<div id="timbre_blanc">
+    				<img src="img/timbre.png" alt="timbre décoratif">	
+    			</div>
     		</div>
-    		</div>
-
 		</div>
-
 	</body>
     <?php include ('footer.php'); ?>
 </html>
