1fe24dd10adec2b42c52b6063204aff707f89bec
Julien <Julien.goisset@gmail.com>
2021-05-31 17:21:04 +0200

    merge

--- page-type.php
+++ page-type.php
@@ -8,7 +8,104 @@
 	</head>
     <?php include ('header.php'); ?>
 	<body>
-
+		<img src="img/himeji.png" alt="photo paysage" id="fullpageimg">
+		<div id="left-panel">
+			<h2>Japon</h2>
+			<img src="img/carte.png" alt="carte du Japon">
+			<h3>Continent :</h3>
+			<p>Asie</p>
+			<h3>Capitale :</h3>
+			<p>Tokyo</p>
+			<h3>Langue :</h3>
+			<p>japonais</p>
+			<div id="anecdote_container">
+				<div id="bloc1">
+					<div class="fond_blanc">
+						<img src="img/capsule.png" alt="photo hôtel capsule">
+					</div>
+					<div class="fond_blanc2">
+					</div>
+				</div>
+				<p>Le premier hôtel capsule a ouvert ses portes le 1er février 1979 à Osaka.</p>
+				<div id="bloc2">
+					<div class="fond_blanc">
+						<img src="img/no.png" alt="photo acteur théâtre Nô">	
+					</div>
+					<div class="fond_blanc2">
+					</div>
+				</div> 
+				<p>Les acteurs du théâtre Nô sont uniquement des hommes et la plupart des pièces tournent au tragique.</p>
+			</div>
+		</div>
+		<div id="content">
+			<div id="presentation">
+				<h2>Le pays du soleil levant</h2>
+				<p>Le Japon (en japonais : 日本) est un pays asiatique au large de la Russie et de la Corée dont la capitale est Tokyo. L'archipel japonais couvre 370 000 km². Le Japon est aussi appelé « pays du soleil levant », parce qu'il se trouve sur un archipel situé juste à l'est du centre géographique de l'Asie (le Xinjiang, en Chine). Pays montagneux, le Japon renferme de très nombreux volcans et est régulièrement secoué par des séismes. Ses habitants sont les Japonais, qui sont environ 127 millions. La natalité étant très faible et l'immigration quasi inexistante, la population japonaise commence à diminuer. Très grand pays industriel, le Japon est l'un des États les plus riches et les plus développés du monde, il appartient d'ailleurs à l'organisation G7.
+				</p>
+			</div>
+			<h3>Gastronomie japonaise</h3>
+			<div id="conteneur-plats">
+				<div id="img-gauche">
+					<img src="img/takoyaki.png" alt="photo takoyaki">
+					<h4>Le takoyaki</h4>
+					<p>Mets de la cuisine japonaise classé dans les konamon et une spécialité d'Osaka, se présentant sous forme de boulettes de pâte, semblable à la pâte à crêpe, contenant des morceaux de poulpe, cuites en moule, comme les gaufres. 
+					</p>
+				</div>
+				<div id="img-droite">
+					<img src="img/okonomiyaki1.png" alt="photo okonomiyaki">
+					<h4>L'okonomiyaki</h4>
+					<p>Ce plat japonais est composé d'une pâte qui enrobe un nombre d'ingrédients très variables découpés en petits morceaux, le tout étant cuit sur une plaque chauffante. Il est souvent servi dans des restaurants spécialisés, où il est parfois possible de le réaliser soi-même.
+					</p>
+				</div>
+			</div>  
+			<h3> Villes à visiter </h3>
+			<div id="conteneur-villes">
+				<div id="ville">
+					<img src="img/nara.png" alt="photo Nara">
+					<h5>Nara</h5>
+					<p>Au centre-sud de l'île d'Honshū, Nara est la capitale de la préfecture japonaise du même nom. La ville abrite des œuvres d'art et des temples importants qui remontent au VIIIe siècle, lorsqu'elle était la capitale du Japon.
+					</p> 
+				</div>
+				<div id="ville">
+					<img src="img/hokkaido.png" alt="photo Hokkaido">
+					<h5>Hokkaido</h5>
+					<p>Hokkaido est la plus septentrionale des quatre îles principales de l'archipel du Japon. Parmi les japonais qui y vivent se trouvent une minorité d'Aïnous, qui furent les premiers habitants de l'archipel.
+					</p> 
+ 				</div>
+				<div id="ville">
+					<img src="img/okinawa.png" alt="photo Okinawa">
+					<h5>Okinawa</h5>
+					<p>Ile de l'archipel Ryukyu, Okinawa est une ville dont les habitants soulignent avec force leur passé de royaume indépendant et leur différence culturelle avec le Japon. La région est connue pour ses plages et la beauté de ses eaux.
+					</p> 
+				</div>
+			</div>
+			<div id="galerie_container">
+				<div class="rectangle_blanc">
+					<img src="img/galerie1.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie2.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie3.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie4.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie5.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie6.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie7.png" alt="photo paysage">	
+				</div>
+				<div class="rectangle_blanc">
+					<img src="img/galerie8.png" alt="photo paysage">	
+				</div>
+			</div>
+		</div>
 	</body>
     <?php include ('footer.php'); ?>
 </html>
